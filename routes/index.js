c0f8555498f78d143a92346def0d89417c7e7def
Håvard Andersen <havard.stigen.andersen@gmail.com>
2018-05-28 21:04:07 +0200

    skall

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
