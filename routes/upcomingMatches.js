d00c96ebc25797ddb959b559e00cb422738d85e6
Håvard Andersen <havard.stigen.andersen@gmail.com>
2018-06-03 23:52:11 +0200

    Kan lagre betten

--- routes/upcomingMatches.js
+++ routes/upcomingMatches.js
@@ -7,13 +7,18 @@ const knex = require('knex')(knexConfig);
 
 const router = express.Router();
 
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
 async function showUpcoming(req, res) {
   const upcoming = (await knex.raw(`
 SELECT m.game_start, m.away_team, m.home_team, b.home_score, b.away_score, b.match_id, b.id bet_id, m.channel
 FROM bets b, matches m
 WHERE b.user_id = ?
 AND b.match_id = m.id
-ORDER BY game_start asc;`,
+AND game_start > now()
+ORDER BY game_start, m.id asc;`,
     [req.user.id + ''])).rows;
 
   res.render('upcomingMatches', { username: req.user.name, upcoming });
@@ -23,7 +28,37 @@ router.get('/', asyncHandler(async (req, res) => {
   await showUpcoming(req, res);
 }));
 
+function isValidMatch(validMatches, id) {
+  //TODO implement
+  return true;
+}
+
 router.post('/', asyncHandler(async (req, res) => {
+
+  const validMatches = (await knex.raw(`
+SELECT id
+FROM matches m
+WHERE game_start > now();`)).rows;
+
+  for (const key in req.body) {
+    let score = req.body[key];
+    if (score === '' || isNumeric(score)) {
+      if (score === '') {
+        score = null;
+      }
+      const team = key.startsWith('home') ? 'home' : 'away';
+      const id = key.replace('home-', '').replace('away-', '');
+      if (isValidMatch(validMatches, id)) {
+        const bet = {};
+        bet[`${team}_score`] = score;
+
+        await knex('bets')
+          .where('user_id', '=', req.user.id)
+          .where('match_id', '=', id)
+          .update(bet)
+      }
+    }
+  }
 
   await showUpcoming(req, res);
 }));
