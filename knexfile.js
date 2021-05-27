714a48c9c7eead655a5de19938dc1b9f5e1f4495
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2021-05-27 07:41:17 +0200

    ssl

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ const config = require('config');
 
 module.exports = {
   client: 'pg',
-  connection: config.get('databaseUrl'),
+  connection: config.get('databaseUrl') + "?ssl=true",
   migrations: {
     tableName: 'knex_migrations',
   },
