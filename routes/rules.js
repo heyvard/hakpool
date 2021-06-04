d84a2c42072c4521f4219c60b42ca372b37800e2
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2021-06-04 10:10:37 +0200

    Skjul innskudd hak

--- routes/rules.js
+++ routes/rules.js
@@ -2,11 +2,12 @@
 
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const config = require('config');
 
 const router = express.Router();
 
 router.get('/', asyncHandler(async (req, res) => {
-  res.render('rules', { user: req.user });
+  res.render('rules', { user: req.user, visInnskudd: config.get('appname') === "em2021-betpool", });
 }));
 
 
